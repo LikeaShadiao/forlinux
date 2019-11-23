07bc6e7f17012ecfeb9fe676a9c81777f7dca1c5
ltzzfeeling <2401244500@qq.com>
2019-11-23 15:21:23 +0800

    finishqueue

--- queue.c
+++ queue.c
@@ -89,4 +89,137 @@ typedef struct
 	QueueNode *rear,*front;
 }LinkQueue;
 
-//
+//shengcheng konglian duilie
+void InitQueue(LinkQueue*&q)
+{
+	q=(LinkQueue*)malloc(sizeof(LinkQueue));
+	q->front=q->rear=(QueueNode*)malloc(sizeof(QueueNode));
+	q->front->next=NULL;
+}
+
+//zhikong
+void SetNull(LinkQueue*q)
+{
+	q->rear=q->front;
+	q->front->next=NULL;
+}
+
+//judge if empty
+int Empty(LinkQueue*q)
+{
+	if(q->front==q->rear)
+		return 1;
+	return 0;
+}
+
+//rudui of linkqueue
+void Enqueue(LinkQueue*q,datatype x)
+{
+	q->rear->next=(QueueNode*)malloc(sizeof(QueueNode));
+	q->rear=q->rear->next;
+	q->rear->data=x;//not "q->data[q->rear]=x;"
+}
+
+//chulie of linkqueue
+void Dequeue(LinkQueue*q,datatype&x)
+{
+	if(q->rear==q->front)
+	{
+		printf("duilie xiayi ");
+	}
+	else
+	{
+		QueueNode*s=q->front->next;
+		if(s->next==NULL)
+		{
+			q->front->next=NULL;
+			q->front=q->rear;
+		}
+		else
+		{
+			q->front->next=s->next;
+		}
+		x=s->data;
+		free(s);
+	}
+}
+
+//qv duitou element
+int Getduitou(LinkQueue*q,datatype&x)
+{
+	if(Empty(q))
+	{
+		printf("duilie xiayv");
+		return 0;
+	}
+	else
+	{
+		x=q->front->next->data;
+		return 1;
+	}
+}
+
+//use circle linklist with headnode behalf queue
+//before chudui compare the length with 1
+//rudui
+void Enqueue(QueueNode*&rear,datatype x)
+{
+	QueueNode*p=(QueueNode*)malloc(sizeof(QueueNode));
+	p->data=x;
+	p->next=rear->next;
+	rear->next=p;
+	rear=p;
+}
+
+//chudui
+void Dequeue(QueueNode*&rear,datatype&x)
+{
+	QueueNode*p;
+	if(rear->next==rear)
+	{
+		printf("duixaiyi");
+	}
+	else
+	{
+		p=rear->next->next;
+		x=p->data;
+		if(p==rear)
+		{
+			rear=rear->next;
+			rear->next=rear;
+		}
+		else
+			rear->next->next=p->next;
+		free(p);
+	}
+}
+
+//using two sequent stack to realize queue
+void Enqueue(SqStack*s1,datatype x)
+{
+	if(s1->Top==maxsize-1)
+		printf("shangyi");
+	else
+		Push(s1,x);
+}
+
+datatype Dequeue(SqStack*s1)
+{
+	SqStack *s2;
+	datatype x;
+	InitStack(s2);
+	while(!Empty(s1))
+		Push(s2,Pop(s1));//yeah Pop is another function you need to write
+	x=Pop(s2);
+	while(!Empty(s2))
+		Push(s1,Pop(s2));
+}
+
+int queue_empty(SqStack*s1)
+{
+	if(Empty(s1))
+		return 1;
+	return 0;
+}
+
+//Then may be example of apply,but I want to do other work now.
